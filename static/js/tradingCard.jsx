5a8cca6d8b7ba53b85298dc1ab6629773024ccb3
Sydney Holden <scholdenak@gmail.com>
2020-12-27 22:23:02 +0000

    added card container

--- static/js/tradingCard.jsx
+++ static/js/tradingCard.jsx
@@ -49,17 +49,17 @@ const tradingCardData = [
     imgUrl: '/static/img/merge.jpg'
   },
 
-  // {
-  //   name="Shortstack Overflow",
-  //   skill="being delicous",
-  //   imgUrl="/static/img/shortstack-overflow.jpg"
-  // },
+  {
+    name: "Shortstack Overflow",
+    skill: "being delicous",
+    imgUrl: "/static/img/shortstack-overflow.jpg"
+  },
 
-//   {
-//     name="Off By One"
-//     skill="navigating neo's belly-button",
-//     imgUrl="/static/img/off-by-one.jpg",
-//  }
+  {
+    name: "Off By One",
+    skill: "navigating neo's belly-button",
+    imgUrl: "/static/img/off-by-one.jpg"
+ },
 ];
 
 function TradingCard(props) {
@@ -72,57 +72,83 @@ function TradingCard(props) {
   );
 }
 
-ReactDOM.render(
-  (
-    <TradingCard
-      name="Balloonicorn"
-      skill="video games"
-      imgUrl="/static/img/balloonicorn.jpg"
-    />
-  ),
-  document.querySelector('#balloonicorn')
-);
+function TradingCardContainer() {
+  return (
+    <div>
+      success!
+    </div>
+  );
+}
 
 ReactDOM.render(
-  (
-    <TradingCard
-      name="Float"
-      skill="baking pretzels"
-      imgUrl="/static/img/float.jpg"
-    />
-  ),
-  document.querySelector('#float')
+  <TradingCardContainer />, document.querySelector('#container')
 );
 
-ReactDOM.render(
-  (
-    <TradingCard
-      name="Llambda"
-      skill="knitting scarves"
-      imgUrl="/static/img/llambda.jpg"
-    />
-  ),
-  document.querySelector('#llambda')
-);
+// for (const currentCard of tradingCardData) {
+//   (currentCard.name, currentCard.skill);
+// }
+// ReactDOM.render(
+//   (
+//     <TradingCard
+//       name="Balloonicorn"
+//       skill="video games"
+//       imgUrl="/static/img/balloonicorn.jpg"
+//     />
+//   ),
+//   document.querySelector('#balloonicorn')
+// );
 
-ReactDOM.render(
-  (
-    <TradingCard
-      name="Off By One"
-      skill="navigating neo's belly-button"
-      imgUrl="/static/img/off-by-one.jpg"
-    />
-  ),
-  document.querySelector('#off-by-one')
-);
+// ReactDOM.render(
+//   (
+//     <TradingCard
+//       name="Float"
+//       skill="baking pretzels"
+//       imgUrl="/static/img/float.jpg"
+//     />
+//   ),
+//   document.querySelector('#float')
+// );
 
-ReactDOM.render(
-  (
-    <TradingCard
-      name="Shortstack Overflow"
-      skill="being delicous"
-      imgUrl="/static/img/shortstack-overflow.jpg"
-    />
-  ),
-  document.querySelector('#shortstack-overflow')
-);
+// ReactDOM.render(
+//   (
+//     <TradingCard
+//       name="Llambda"
+//       skill="knitting scarves"
+//       imgUrl="/static/img/llambda.jpg"
+//     />
+//   ),
+//   document.querySelector('#llambda')
+// );
+
+// ReactDOM.render(
+//   (
+//     <TradingCard
+//       name="Off By One"
+//       skill="navigating neo's belly-button"
+//       imgUrl="/static/img/off-by-one.jpg"
+//     />
+//   ),
+//   document.querySelector('#off-by-one')
+// );
+
+// ReactDOM.render(
+//   (
+//     <TradingCard
+//       name="Shortstack Overflow"
+//       skill="being delicous"
+//       imgUrl="/static/img/shortstack-overflow.jpg"
+//     />
+//   ),
+//   document.querySelector('#shortstack-overflow')
+// );
+
+// ReactDOM.render(
+//   (
+//     <TradingCard
+//       name="Console.log"
+//       skill="being warm"
+//       imgUrl="/static/img/shortstack-overflow.jpg"
+//     />
+//   ),
+//   document.querySelector('#consoledotlog')
+// );
